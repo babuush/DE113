25d904e3606bac3344301bd6fe3e63ca8b4ddb36
babuush <101738397+babuush@users.noreply.github.com>
2024-02-02 18:25:15 +0700

    Add files via upload

---
+++ lab1/Window.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Window {
+    public static void main(String[] args) {
+        JFrame myWindow;
+        myWindow = new JFrame();
+        myWindow.setSize(300, 200);
+        myWindow.setTitle("6610Thanapat");
+        myWindow.setVisible(true);
+    }
+}
