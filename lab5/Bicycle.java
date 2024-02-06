04a81bb85e54e96ea3183f0c057893feb1964aed
babuush <thanapatpiyamassakul@gmail.com>
2024-02-07 00:05:02 +0700

    update file

--- lab5/Bicycle.java
+++ lab5/Bicycle.java
@@ -1,9 +1,11 @@
 public class Bicycle {
     private String ownerName;
+    private int tagNo; // Adding tag number as a new attribute
 
     // Constructor
     public Bicycle() {
         ownerName = "";
+        tagNo = 0; // Initializing tag number to 0 by default
     }
 
     // Getter method for ownerName
@@ -14,5 +16,15 @@ public class Bicycle {
     // Setter method for ownerName
     public void setOwnerName(String ownerName) {
         this.ownerName = ownerName;
+    }
+
+    // Getter method for tagNo
+    public int getTagNo() {
+        return tagNo;
+    }
+
+    // Setter method for tagNo
+    public void setTagNo(int tagNo) {
+        this.tagNo = tagNo;
     }
 }
