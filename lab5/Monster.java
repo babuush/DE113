123df8c3a5392dd5905dd7cd64e8c01231eae239
babuush <101738397+babuush@users.noreply.github.com>
2024-02-07 11:28:27 +0700

    Update Monster.java

--- lab5/Monster.java
+++ lab5/Monster.java
@@ -3,16 +3,10 @@ public class Monster {
     private final int attackPow;
     private int health;
 
-    // Constructor
     public Monster(String name, int attackPow) {
         this.name = name;
         this.attackPow = attackPow;
         this.health = 100;
-    }
-
-    // Getter methods
-    public int getHealth() {
-        return health;
     }
 
     public String getName() {
@@ -23,20 +17,19 @@ public class Monster {
         return attackPow;
     }
 
-    // Method to deduct health when attacked by another monster
-    public void attackedBy(Monster m) {
-        health -= m.getAttackPow();
-        if (health < 0) {
-            health = 0; // Ensure health doesn't go below 0
+    public int getHealth() {
+        return health;
+    }
+
+    public void attackedBy(Monster attacker) {
+        this.health -= attacker.attackPow;
+        if (this.health < 0) {
+            this.health = 0;
         }
     }
 
-    // Method to heal the monster
     public void healed() {
-        health += 8;
-        if (health > 100) {
-            health = 100; // Ensure health doesn't exceed 100
-        }
+        this.health += 8;
     }
 
     public static void main(String[] args) {
@@ -46,8 +39,8 @@ public class Monster {
         m2.attackedBy(m1);
         m2.attackedBy(m1);
         m2.healed();
-        System.out.println(m1.getName() + "'s current health is" + m1.getHealth()); //⇒ Pikachu’s current health is …
-        System.out.println(m2.getName() + "'s current health is" + m2.getHealth()); //⇒ Eevee‘s current health is …
+        System.out.println(m1.getName() + "'s current health is " + m1.getHealth()); //⇒ Pikachu’s current health is …
+        System.out.println(m2.getName() + "'s current health is " + m2.getHealth()); //⇒ Eevee‘s current health is …
 
     }
 }
