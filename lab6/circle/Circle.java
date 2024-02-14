0763979c86cae63293e789eab36cc12f42a613b9
babuush <101738397+babuush@users.noreply.github.com>
2024-02-14 13:55:48 +0700

    Add files via upload

---
+++ lab6/circle/Circle.java
@@ -0,0 +1,33 @@
+package circle;
+
+public class Circle {
+    private double radius;
+
+    public Circle() {
+        this(1.0);
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public String toString() {
+        return "Circle[radius=" + radius + "]";
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}
