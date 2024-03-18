6a7ef9e09c781e8a59030750399e8423d2a82b01
babuush <thanapatpiyamassakul@gmail.com>
2024-03-18 23:40:59 +0700

    Mod problem11.java

--- homework8/problem11.java
+++ homework8/problem11.java
@@ -4,35 +4,37 @@ public class problem11 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Sentence: ");
-        String sentence = scanner.nextLine();
+        while (true) {
+            System.out.print("Enter a sentence: ");
+            String sentence = scanner.nextLine().trim();
 
-        // Loop until an empty sentence is returned.
-        while (!sentence.isEmpty()) {
-
-            // Variables to keep a count of the vowels.
-            int a = 0, e = 0, i = 0, o = 0, u = 0;
-
-            // Scan each character in the sentence.
-            for (int c = 0; c < sentence.length(); c++) {
-                char ch = Character.toLowerCase(sentence.charAt(c));
-                if (ch == 'a') a++;
-                if (ch == 'e') e++;
-                if (ch == 'i') i++;
-                if (ch == 'o') o++;
-                if (ch == 'u') u++;
+            if (sentence.isEmpty()) {
+                System.out.println("Exiting...");
+                break;
             }
 
-            // Display the result.
-            System.out.println("Vowel counts for " + sentence);
-            System.out.println("Number of 'a': " + a);
-            System.out.println("Number of 'e': " + e);
-            System.out.println("Number of 'i': " + i);
-            System.out.println("Number of 'o': " + o);
-            System.out.println("Number of 'u': " + u);
-            System.out.println("Enter next Sentence: ");
-            sentence = scanner.nextLine();
+            printResult(sentence);
         }
-        System.out.println("Entered an empty input. Exiting now...");
+
     }
+
+    public static void printResult(String sentence) {
+        int[] vowelCounts = new int[5];
+
+        for (char ch : sentence.toCharArray()) {
+            if (ch == 'a') vowelCounts[0]++;
+            else if (ch == 'e') vowelCounts[1]++;
+            else if (ch == 'i') vowelCounts[2]++;
+            else if (ch == 'o') vowelCounts[3]++;
+            else if (ch == 'u') vowelCounts[4]++;
+        }
+
+        System.out.println("Vowel counts for '" + sentence + "':");
+        System.out.println("Number of 'a': " + vowelCounts[0]);
+        System.out.println("Number of 'e': " + vowelCounts[1]);
+        System.out.println("Number of 'i': " + vowelCounts[2]);
+        System.out.println("Number of 'o': " + vowelCounts[3]);
+        System.out.println("Number of 'u': " + vowelCounts[4]);
+    }
+
 }
