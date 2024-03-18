8279aaccc540a32031c301b11a59b647c54d007a
babuush <thanapatpiyamassakul@gmail.com>
2024-03-18 23:08:16 +0700

    Mod problem10.java

--- homework8/problem10.java
+++ homework8/problem10.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class problem10 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+
+        while (!sentence.isEmpty())  {
+            StringBuffer reversedSentence = new StringBuffer(sentence).reverse();
+            System.out.println("reversed sentence = " + reversedSentence);
+
+            System.out.print("Enter a sentence: ");
+            sentence = scanner.nextLine();
+        }
+        System.out.println("Entered an empty string. Exiting now ...");
+    }
 }
