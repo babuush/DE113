692483fb426645419164055b17875dd3ae023f0d
babuush <thanapatpiyamassakul@gmail.com>
2024-03-18 23:00:17 +0700

    Mod problem9.java

--- homework8/problem9.java
+++ homework8/problem9.java
@@ -4,18 +4,19 @@ public class problem9 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a character: ");
-        String c = scanner.nextLine();
+        String inputVariable = scanner.nextLine();
 
-        while ((int) c.charAt(0) != 64) {
-            int length = c.length();
+        while ((int) inputVariable.charAt(0) != 64) {
+            int length = inputVariable.length();
             if (length > 1) {
                 System.out.println("Error: more than one character entered");
             } else {
-                char check = c.charAt(0);
-                System.out.println("The ASCII value of " + check + " is " + (int) check);
+                char check = inputVariable.charAt(0);
+                System.out.println("The ASCII v" +
+                        "alue of " + check + " is " + (int) check);
             }
             System.out.print("Enter a character: ");
-            c = scanner.nextLine();
+            inputVariable = scanner.nextLine();
         }
         System.out.println("You entered @. Exiting now ...");
     }
