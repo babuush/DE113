bdeca94fb0602f29f989c3e0e5cdade6b1a2ded6
babuush <thanapatpiyamassakul@gmail.com>
2024-03-20 09:10:38 +0700

    Add exercise1.java

---
+++ lab10/exercise1.java
@@ -0,0 +1,9 @@
+public class exercise1 {
+
+    public static void main(String[] args) {
+        int[] myArray;
+        myArray = new int[10];
+        char[] sentence = new char[100];
+        double[] teamPoints = new double[5];
+    }
+}
