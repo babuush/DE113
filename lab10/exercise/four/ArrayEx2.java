1995b48971e6a7cea64bc0a472494bb08e7137d2
babuush <thanapatpiyamassakul@gmail.com>
2024-03-20 09:59:05 +0700

    Add ArrayEx2.java

--- lab10/exercise/four/ArrayEx2.java
+++ lab10/exercise/four/ArrayEx2.java
@@ -1,4 +1,17 @@
 package exercise.four;
 
 public class ArrayEx2 {
+
+    public static void main(String[] args) {
+        String[] browsers = new String[4];
+        browsers[0] = "explorer";
+        browsers[1] = "chrome";
+        browsers[2] = "opera";
+        browsers[3] = "firefox";
+        System.out.print("size of array: " + browsers.length);
+        for (String browser : browsers) {
+            System.out.println(browser);
+
+        }
+    }
 }
