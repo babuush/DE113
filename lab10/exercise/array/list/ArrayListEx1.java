52957192e2b99116a807f49299de531139263a90
babuush <thanapatpiyamassakul@gmail.com>
2024-03-20 11:40:32 +0700

    Mod ArrayListEx1.java

--- lab10/exercise/array/list/ArrayListEx1.java
+++ lab10/exercise/array/list/ArrayListEx1.java
@@ -1,7 +1,17 @@
 package exercise.array.list;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ArrayListEx1 {
     public static void main(String[] args) {
+        ArrayList students = new ArrayList();
+        students.addAll(Arrays.asList("Amy", "Bob", "Cindy", "David"));
+        students.add(0,"Nick");
+        students.add(1,"Mike");
+        students.remove(3);
+        System.out.println("Students are: "+ students.toString());
+        System.out.println("There are " + students.size() + " students");
 
     }
 }
