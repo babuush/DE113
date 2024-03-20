dbe5d3af09fe10ff99815bf367e80f72731de6b1
babuush <thanapatpiyamassakul@gmail.com>
2024-03-20 09:52:34 +0700

    Mod ComputeAvg.java

--- lab10/exercise/three/ComputeAvg.java
+++ lab10/exercise/three/ComputeAvg.java
@@ -1,7 +1,19 @@
 package exercise.three;
+
 import java.util.Scanner;
+
 public class ComputeAvg {
     public static void main(String[] args) {
+        double[] scoreArray = new double[5];
+        double averageScore = 0, sum = 0;
+        Scanner scanner = new Scanner(System.in);
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter the score " + i);
+            scoreArray[i] = scanner.nextInt();
+            sum += scoreArray[i];
+        }
+        averageScore = sum / 5;
+        System.out.println("Average test score is:" + averageScore);
     }
 }
